c7d1519c8bbaa156dffe09dfe82959d7d42ab3a9
abysito93 <abdiel.moran@upr.edu>
2016-05-11 23:08:40 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var assert = require('assert');
+var passport = require('passport');
+var mongoose = require('mongoose');
+var jwt = require('jwt-simple');
+var bcrypt = require('bcrypt');
+var config = require('./public/javascripts/database');
+var User = require('./public/javascripts/register');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var game = require('./routes/game');
+var register = require('./routes/register');
+var login = require('./routes/login');
+var register = require('./routes/register')
+var register = require('./routes/register')
+var register = require('./routes/register')
 
 var app = express();
 
@@ -20,10 +34,91 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/index', routes);
 app.use('/users', users);
+app.use('/game', game);
+app.use('/register', register);
+app.use('/login', login);
+app.post('/register', function (req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({succes: false, msg: 'Please enter name and password'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password,
+      exp: 0
+    });
+    newUser.save(function (err) {
+      if (err) {
+        res.json({succes: false, msg: 'Username aleady exist'});
+      }
+      else {
+        res.json({succes: true, msg: 'Successful created user'});
+      }
+    });
+    }
+  });
+
+  app.post('/login', function (req, res) {
+    User.findOne({
+      name: req.body.name
+    }, function (err, user) {
+      if (err) throw err;
+      if (!user) {
+        return res.status(403).send({success: false, msg: "Authentication failed, User not found"});
+      } else {
+        user.comparePassword(req.body.password, function(err, isMatch) {
+          if (isMatch && !err) {
+            var token = jwt.encode(user, config.secret);
+            res.json({succes: true, token: 'JWT ' + token});
+        } else {
+          return res.status(403).send({success: false, msg: "Authentication failed, Wrong password"});
+        }
+      });
+      }
+    });
+  });
+
+  app.get('/users', passport.authenticate('jwt', {session: false}), function (req, res) {
+    var token = getToken(req.headers);
+
+    if (token) {
+      var decoded = jwt.decode(token, config.secret);
+      User.findOne({
+        name: decoded.name
+      }, function (err, user) {
+        if (err) throw err;
+
+        if (!user) {
+          return res.status(403).send({ success: false, msg: "Authentication failed, User not found"});
+        } else {
+          return res.json({success: true, msg: 'Welcome to your profile ' + user.name + '!'});
+          }
+      });
+    }
+    else {
+      return res.status(403).send({success: false, msg: "No token provided"});
+    }
+  });
+
+  getToken = function(headers) {
+    if (headers && headers.authorization) {
+      var parted =  headers.authorization.split(' ');
+      if (parted.lenght ===2) {
+        return parted[1];
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  };
+
+mongoose.connect(config.database);
+require('./public/javascripts/passport')(passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
