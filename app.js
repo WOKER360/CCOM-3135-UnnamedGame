38b132eae43262b77a73b1682ba5f6ca8f95e138
WOKER360 <woker360@gmail.com>
2016-05-12 14:07:52 -0400

    added game

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var game = require('./routes/game');
-var register = require('./routes/register')
+var register = require('./routes/register');
+var players = require('./routes/player');
 
 var app = express();
 
@@ -28,7 +29,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/game', game);
 app.use('/register', register);
-
+app.use('/players', players);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
