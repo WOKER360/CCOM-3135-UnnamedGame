34a8f95ecbde5df7d22e9183ad352a1726b9af37
abysito93 <abdiel.moran@upr.edu>
2016-05-11 23:07:25 -0600

    Add files via upload

---
+++ routes/register.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* GET register page. */
+router.get('/', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
+
+module.exports = router;
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+module.exports = router;
